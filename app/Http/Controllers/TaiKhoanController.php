87a26143665049243ff021b080c177ea986e801e
provip987 <dotrongkhiemvippro@gmail.com>
2024-06-05 18:20:46 +0700

    dang nhap

---
+++ app/Http/Controllers/TaiKhoanController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\TaiKhoan;
+use Illuminate\Http\Request;
+
+
+class TaiKhoanController extends Controller
+{
+    public function dangNhap()
+    {
+      return View('tai_khoan/dang-nhap');
+    }
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TaiKhoanController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/DangNhap', [TaiKhoanController::class, 'dangNhap'])->name('DangNhap');
